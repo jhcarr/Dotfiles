f443811f241db87a2cf438abcd2d00fa82b8d1b7
Justin Carr <justin.carr@puppetlabs.com>
2013-07-30 16:59:15 -0700

    (maint) BROKEN Refactor _check2 to use safe file iteration
    This commit represents work done intermittently on the work laptop
    and is in a broken state.
    
    This commit introduces safe file checking via a file name array where
    each entry is a valid filename ending with a null character.

--- src/.emacs.d/init.el
+++ src/.emacs.d/init.el
@@ -9,25 +9,33 @@
 (package-initialize)
 
 ;;; If packages missing, download and install
-
 (defvar my-packages '(starter-kit
-                      ;starter-kit-ruby
+                      starter-kit-ruby
                       starter-kit-lisp
                       starter-kit-bindings
                       starter-kit-eshell
                       clojure-mode
                       clojure-test-mode
+                      rainbow-delimiters
                       nrepl
-                      )
-  "A list of packages to be compiled at runtime.")
-
-(when (not package-archive-contents) (package-refresh-contents))
+                      auto-complete ac-nrepl
+                      ))
 
 (dolist (p my-packages)
-  (when (not (package-installed-p p))(package-install p))
+  (when (not (package-installed-p p))
+    (message "%s" "Refreshing package database...")
+    (package-refresh-contents)
+    (message "%s" "...done.")
+    (package-install p)
+    )
   )
 
 ;;; Line numbering mode
 (setq linum-format "%2d |")
 (global-linum-mode t)
+
+;;; Clojure mode rainbow delimiters
+;;(add-to-list 'load-path ~/.emacs.d/rainbow-delimiters.elc)
+(require 'rainbow-delimiters)
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
 
